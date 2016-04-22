5577df09349ab15e5647da85f79eb297eda09715
Carolina Cerro <carolbcn88@gmail.com>
2016-04-22 20:12:46 +0200

    Modificación HTML's

--- public/core.js
+++ public/core.js
@@ -1,8 +1,12 @@
 angular.module('MainApp', [])
+var Persona = require('./modelo/user');
+var Rutas = require('./modelo/route');
 
 function mainController($scope, $http) {
     $scope.newPersona = {};
     $scope.personas = {};
+    $scope.newRuta  = {};
+    $scope.rutas    = {};
     $scope.selected = false;
 
     // Obtenemos todos los datos de la base de datos
@@ -64,14 +68,57 @@ function mainController($scope, $http) {
         console.log($scope.newPersona, $scope.selected);
     };
 
-    $scope.verificar = function verificar(v){
-        var p1 = document.getElementById('pass1');
-        if( p1.value != v){
-            document.getElementById('mensaje').innerHTML = "no coincide";
 
-        }else{
-            document.getElementById('mensaje').innerHTML = "ok";
-            return true;
-        }
-    }
+    // Obtenemos todos los datos de la base de datos de todas las rutas
+    $http.get('/routes').success(function (data) {
+            $scope.rutas = data;
+        })
+        .error(function (data) {
+            console.log('Error: ' + data);
+        });
+
+    // Función para registrar una Ruta
+    $scope.registrarRuta = function () {
+        $http.post('/route', $scope.newRuta)
+            .success(function (data) {
+                $scope.newRuta = {}; // Borramos los datos del formulario
+                $scope.rutas   = data;
+            })
+            .error(function (data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+
+
+    // Función para editar los datos de una Ruta
+    $scope.modificarRuta = function (newRuta) {
+        $http.put('/route/update', $scope.newRuta)
+            .success(function (data) {
+                $scope.newRuta  = {}; // Borramos los datos del formulario
+                $scope.rutas    = data;
+                $scope.selected = false;
+            })
+            .error(function (data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    // Función que borra una Ruta conocido su el ID
+    $scope.borrarRuta = function (name, res) {
+        if (confirm ("¿Seguro que quieres eliminar? "))
+
+        {
+              $http.delete('/route/delete/' + name)
+                .success(function (data) {
+                    $scope.newRuta  = {};
+                    $scope.rutas    = data;
+                    $scope.selected = false;
+                })
+                .error(function (data) {
+                    console.log('Error: ' + data);
+                });}
+    };
+
+
 }
